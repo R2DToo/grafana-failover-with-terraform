36e28e513a9d324320447db1567ec63c70757568
LegendOfGrudge <nicholasfletcher3682@yahoo.ca>
2024-11-26 15:18:49 -0600

    Creates a Terraform file based on dashboards and folder structure

--- index.js
+++ index.js
@@ -190,9 +190,124 @@ async function setupChildDashboardRepo(parentFolder, parentFolderPath) {
   }
 }
 
+async function createTerraformFile() {
+  let folderIndex = 0;
+  let dashboardIndex = 0;
+
+  let terraform = `
+terraform {
+  required_providers {
+      grafana = {
+        source  = "grafana/grafana"
+        version = ">= 2.9.0"
+      }
+  }
+}
+
+provider "grafana" {
+  alias = "stack"
+
+  url  = "${API_HOST}"
+  auth = "${API_KEY}"
+}
+  `;
+
+  for(let item in folders) {
+    if (folders[item].type === "folder")
+    {
+      const folderPath = `dashboards/${folders[item].title}`;
+      const folderConfig = `
+
+// ${folders[item].title} Folder
+
+resource "grafana_folder" "Folder${folderIndex}" {
+  provider = grafana.stack
+
+  title = "${folders[item].title}"
+}
+      `;
+      terraform = terraform + folderConfig;
+      terraform = await createChildTerraformConfig(terraform, folderPath, `Folder${folderIndex}`, folders[item]);
+      folderIndex++;
+    }
+    else if (folders[item].type === "dashboard")
+    {
+      let title = folders[item].title;
+      title = title.replace(/\//g, "-");
+
+      const dashboardConfig = `
+
+// ${title} Dashboard
+
+resource "grafana_dashboard" "Dashboard${dashboardIndex}" {
+  provider = grafana.stack
+
+  config_json = file("${"${path.module}"}/dashboards/${title}.json")
+}
+      `;
+      terraform = terraform + dashboardConfig;
+      dashboardIndex++;
+    }
+  }
+
+  const filePath = path.join(__dirname, 'terraform.tf');
+
+  fs.writeFile(filePath, terraform, (err) => {
+    if (err) {
+      console.error('Error writing Terraform file:', err);
+   } else {
+      console.log('Terraform file created successfully!');
+   }
+  })
+}
+
+async function createChildTerraformConfig(terraform, parentFolderPath, parentFolderIndex, parentFolder) {
+  let childFolderIndex = 0;
+  let childDashboardIndex = 0;
+
+  for (let folder in parentFolder.folders) {
+    const folderPath = `${parentFolderPath}/${parentFolder.folders[folder].title}`;
+    const folderConfig = `
+
+// ${parentFolder.folders[folder].title} Folder
+
+resource "grafana_folder" "${parentFolderIndex}_${childFolderIndex}" {
+  provider = grafana.stack
+
+  parent_folder_uid = grafana_folder.${parentFolderIndex}.uid
+  title = "${parentFolder.folders[folder].title}"
+}
+    `;
+    terraform = terraform + folderConfig;
+    terraform = await createChildTerraformConfig(terraform, folderPath, `${parentFolderIndex}_${childFolderIndex}`, parentFolder.folders[folder]);
+    childFolderIndex++;
+  }
+
+  for (let dashboard in parentFolder.dashboards) {
+    let title = parentFolder.dashboards[dashboard].title;
+    title = title.replace(/\//g, "-");
+
+    const dashboardConfig = `
+
+// ${title} Dashboard
+
+resource "grafana_dashboard" "${parentFolderIndex}_Dashboard${childDashboardIndex}" {
+  provider = grafana.stack
+
+  config_json = file("${"${path.module}"}/${parentFolderPath}/${title}.json")
+  folder      = grafana_folder.${parentFolderIndex}.id
+}
+      `;
+      terraform = terraform + dashboardConfig;
+      childDashboardIndex++;
+  }
+  return terraform;
+}
+
 async function main() {
   await getRootFolders();
   await setupDashboardRepo();
+  await createTerraformFile();
 }
 
 main();
